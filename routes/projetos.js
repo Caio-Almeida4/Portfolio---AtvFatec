f24a92e51abbe4bf41a1146e853810d27709051d
Caio Almeida <104635440+Caio-Almeida4@users.noreply.github.com>
2025-11-27 23:19:58 -0300

    <feat>: pagina projetos com links e descricao

--- routes/projetos.js
+++ routes/projetos.js
@@ -2,7 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('projetos', { title: 'Express' });
+  const projetos = [
+    {
+      name: 'Portfolio',
+      desc: 'Repositório com o código do Portfólio atual',
+      link: 'https://github.com/Caio-Almeida4/Portfolio---AtvFatec'
+    },
+    {
+      name: 'Newe',
+      desc: 'API com objetivo passado pela empresa Newe',
+      link: 'https://github.com/PatoJosefo/API-Synch'
+    },
+    {
+      name: 'Análise de dados para jornalistas',
+      desc: 'API com objetivo de reunir dados sobre COVID-19 para jornalistas',
+      link: 'https://github.com/Equipe-CodeLand/API-2023.1'
+    }
+  ];
+  res.render('projetos', { projetos });
 });
 
 module.exports = router;
